b888c56ea10e0f7c76ffd0512ca1f2d2611f201a
Nathan N <nathansng17@gmail.com>
2022-02-02 17:18:09 -0800

    Added files and broken test

---
+++ SkillDemo.java
@@ -0,0 +1,5 @@
+public class SkillDemo {
+    public static int multi(int x, int y) {
+        return x * y;
+    }
+}
---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void multi() {
+        assertEquals(10, SkillDemo.multi(2, 2));
+    }
+}
